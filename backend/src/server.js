2aebde98abe68f3bde06c035720d4ff1cd109a36
Sriram Annadurai <mailsriram98@gmail.com>
2026-01-10 07:21:31 +0530

    added deployment scripts in package.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,38 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+
 import notesRouter from "./routes/notesRouter.js";
 import { connectDB } from "./config/db.js";
-
 import rateLimiter from "./middleware/rateLimiter.js";
+
 dotenv.config();
+
 const app = express();
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
-);
+const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 app.use("/api/notes", notesRouter);
 
-const PORT = process.env.PORT;
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 connectDB()
     .then(() => {
