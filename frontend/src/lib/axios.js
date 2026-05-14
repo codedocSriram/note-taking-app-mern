e36862cd0e6a74bbc4c24b13d5802a808a1263aa
Sriram Annadurai <42068041+codedocSriram@users.noreply.github.com>
2026-05-14 09:53:23 +0000

    Rectified wrong import and resolved it

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const BASE_URL =
+const BASE_URL =
     import.meta.env.MODE === "development"
         ? "http://localhost:3000/api"
         : "/api";
-const api = axios.create({
+export const api = axios.create({
     baseURL: BASE_URL,
 });
